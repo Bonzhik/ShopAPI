b2cc0bc1b0dfd95ba0eae059a8247fb37599d727
Oleg <bonzhik_04@bk.ru>
2023-11-07 14:59:54 +0300

    RoleFix

--- ShopAPI/Program.cs
+++ ShopAPI/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://localhost:5173") // Здесь укажите домен вашего фронтенда
+        builder.WithOrigins("http://localhost:5175")
             .AllowAnyMethod()
             .AllowAnyHeader();
     });

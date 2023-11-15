980f568a770752cdcbc12e7612aa170f709ddb38
Oleg <bonzhik_04@bk.ru>
2023-11-15 22:13:41 +0300

    3000

--- ShopAPI/Program.cs
+++ ShopAPI/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://localhost:5175")
+        builder.WithOrigins("http://localhost:3000")
             .AllowAnyMethod()
             .AllowAnyHeader();
     });
